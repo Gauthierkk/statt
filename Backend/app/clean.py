e66cc158849d97b81dc99f5c37c8271f6c602ebd
gauthier <gkelly41@gatech.edu>
2025-01-02 14:21:30 -0500

    fixed bug where if the field is missing it crashes

--- Backend/app/clean.py
+++ Backend/app/clean.py
@@ -5,21 +5,27 @@ def clean(data) -> list:
     for row in data:
         cleaned_row = {}
         for key in getKeyValues().keys():
-            if key == 'issue_date':
+            if key not in row:
+                cleaned_row[key] = "no data"
+            elif key == "issue_date":
                 cleaned_row[key] = row[key][:10]
-            else: cleaned_row[key] = row[key]
+            else:
+                cleaned_row[key] = row[key]
         cleaned_data.append(cleaned_row)
 
     return cleaned_data
 
+
 # returns pre-defined list of key values, used to determine which columns to visualize in frontend
 def getKeyValues() -> list:
-    keyValues = {"permittype":"Permit Type",
-                  "permit_type_desc": "Permit Type Description", 
-                  "permit_number": "Permit Number", 
-                  "permit_class": "Permit Class", 
-                  "work_class": "Work Class",
-                  "issue_date": "Issue Date",
-                  "permit_location": "Permit Location",
-                  "description": "Description",}
+    keyValues = {
+        "permittype": "Permit Type",
+        "permit_type_desc": "Permit Type Description",
+        "permit_number": "Permit Number",
+        "permit_class": "Permit Class",
+        "work_class": "Work Class",
+        "issue_date": "Issue Date",
+        "permit_location": "Permit Location",
+        "description": "Description",
+    }
     return keyValues
